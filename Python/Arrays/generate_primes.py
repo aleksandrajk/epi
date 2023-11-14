9f4cd7c62f3799f716bff3fabf2ce2525ce58e6e
Sasha K <55165756+aleksandrajk@users.noreply.github.com>
2023-11-14 07:22:36 +0100

    Update generate_primes.py

--- Python/Arrays/generate_primes.py
+++ Python/Arrays/generate_primes.py
@@ -1,13 +1,18 @@
-# Given n, return all primes up to and including n
+# Given n, return all primes up to and including n.
 def generate_primes(n):
     primes = []
-    # is_prime[p] represents if p is prime or not. At first, set each to true, expecting 0 and 1
-    # Then use sieving to eliminate non-primes.
-    is_prime = [False, False] + [True] * (n - 1)
+    # is_prime[p] represents if p is prime or not
+    is_prime = [False, False] + [True] * (n-1)
     for p in range(2, n + 1):
         if is_prime[p]:
             primes.append(p)
-            # Sieve p's multiples.
+            # Sieve p's multiples
             for i in range(p, n + 1, p):
                 is_prime[i] = False
     return primes
+
+
+result11 = generate_primes(11)
+result64 = generate_primes(64)
+print(result11)  # will print [2, 3, 5, 7, 11]
+print(result64)  # will print [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61]
